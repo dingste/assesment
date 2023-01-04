da445e9b01d71cccef950832c45257ec11eea64c
Dieter Steuten <dieter.steuten@gmail.com>
2023-01-04 10:59:18 +0100

    missing to push last commit - sorry

--- main/assesmentItems.cpp
+++ main/assesmentItems.cpp
@@ -70,10 +70,13 @@ assesmentPoti::assesmentPoti(adc_unit_t adcUnit, adc_channel_t a, adc_channel_t 
 assesmentPoti::~assesmentPoti() {
   // Tear Down
   ESP_ERROR_CHECK(adc_oneshot_del_unit(poti.adc1_handle));
+
+ #if ADC_CALI_SCHEME_CURVE_FITTING_SUPPORTED
   if (poti.calibrated) {
     ESP_ERROR_CHECK(
         adc_cali_delete_scheme_curve_fitting(poti.adc1_cali_handle));
   }
+#endif
 };
 
 bool assesmentPoti::do_adc_calibration(adc_unit_t unit, adc_atten_t atten,
@@ -82,6 +85,7 @@ bool assesmentPoti::do_adc_calibration(adc_unit_t unit, adc_atten_t atten,
   esp_err_t ret = ESP_FAIL;
   bool calibrated = false;
 
+#if ADC_CALI_SCHEME_CURVE_FITTING_SUPPORTED
   if (!calibrated) {
     adc_cali_curve_fitting_config_t cali_config = {
         .unit_id = unit,
@@ -93,7 +97,7 @@ bool assesmentPoti::do_adc_calibration(adc_unit_t unit, adc_atten_t atten,
       calibrated = true;
     }
   }
-
+#endif
   *out_handle = handle;
   return calibrated;
 };
